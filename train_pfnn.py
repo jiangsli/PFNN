1fb7c900eb8c0444533c8e9e34cca25cdf8ea464
jiangsli <jiangsli@stanford.edu>
2020-10-03 16:43:11 -0700

    Optimize import.

--- train_pfnn.py
+++ train_pfnn.py
@@ -12,7 +12,7 @@ from nn.HiddenLayer import HiddenLayer
 from nn.BiasLayer import BiasLayer
 from nn.DropoutLayer import DropoutLayer
 from nn.ActivationLayer import ActivationLayer
-from nn.AdamTrainer import AdamTrainer
+from AdamTrainer import AdamTrainer
 
 rng = np.random.RandomState(23456)
 
